84715981e70d9b7dc9963970cceccd93b1af0220
Farid Kazim <ferid.kazimli@gmail.com>
2025-03-02 18:14:16 +0400

    refactor(core): vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,66 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'node:path'
+import svgr from 'vite-plugin-svgr'
+
+import { version } from './package.json'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	const port = Number(env.VITE_PORT) || 3000;
+
+	return {
+		server: {
+      port,
+      host: true,
+      strictPort: true,
+      allowedHosts: ['app.troya.dev'],
+    },
+		preview: {
+      port,
+    },
+		define: {
+			APP_ENV: JSON.stringify(env.APP_ENV),
+			APP_VERSION: JSON.stringify(version),
+		},
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, 'src'),
+			},
+		},
+		build: {
+			outDir: 'dist',
+			sourcemap: true,
+			target: 'esnext',
+			rollupOptions: {
+				output: {
+					chunkFileNames: '[name]-[hash].js',
+					entryFileNames: '[name]-[hash].js',
+					sourcemapFileNames: '[name]-[hash].js.map',
+				},
+			},
+		},
+		plugins: [
+			react(),
+
+			svgr({
+        include: '**/*.svg',
+        svgrOptions: {
+          plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'prefixIds',
+                params: {
+                  prefixIds: false,
+                  prefixClassNames: false,
+                },
+              },
+            ],
+          },
+        },
+      }),
+		],
+	}
 })
