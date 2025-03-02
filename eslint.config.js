bdecefa013e9d7ae00bf84f097dc63442f6b06f4
Farid Kazim <ferid.kazimli@gmail.com>
2025-03-02 17:36:24 +0400

    refactor(core): esling cache file move to node_modules .cache folder

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  { ignores: ['dist', 'node_modules', 'public'] },
   {
     extends: [tseslint.configs.recommendedTypeChecked, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
